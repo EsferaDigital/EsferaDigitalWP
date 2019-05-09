9d20dd1cd1189850a683102b27d832e682c70953
Gabriel Zavando <gabrielzavando@gmail.com>
2019-05-09 19:12:12 -0400

    comence a trabajar el footer y cambie textos por iconos sociales en el dashboard

--- footer.php
+++ footer.php
@@ -1,9 +1,14 @@
   <footer class="Footer">
     <section class="Footer-container">
-      <div class="Footer-social">
+      <div class="Footer-container-item">
+        <h2 class="Footer-title"> Esfera Digital</h2>
+        <p>Somos una empresa joven y ágil que nace en Abril de 2018 y cuya principal preocupacion es blabla bla Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex fuga atque consequatur distinctio cumque nisi incidunt quam, temporibus quos necessitatibus aspernatur quo numquam excepturi explicabo et natus iusto possimus tempora.</p>
+      </div>
+      <div class="Footer-container-item">
+        <h2 class="Footer-title"> Contacto</h2>
         <?php get_template_part('template-parts/footer-social'); ?>
       </div>
-      <div class="Footer-copy">
+      <div class="Footer-container-item">
         <?php get_template_part('template-parts/footer-copy'); ?>
       </div>
     </section>
